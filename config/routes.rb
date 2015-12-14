cdd5b9f45e489159f247f3a44c7fb5442cde5a2a
Kofi Appiah-Nkansah <kofinkansah@gmail.com>
2015-12-14 02:06:55 +0000

    Implemented student enrollment in courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Flixter::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :courses, :only => [:index, :show]
+  resources :courses, :only => [:index, :show] do
+    resources :enrollments, :only => :create
+  end
   resources :lessons, :only => [:show]
   namespace :instructor do
     resources :sections, :only => [] do
-        resources :lessons, :only => [:new, :create]
+      resources :lessons, :only => [:new, :create]
     end
     resources :courses, :only => [:new, :create, :show] do
-      resources :sections, :only => [:new, :create] 
+      resources :sections, :only => [:new, :create]
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
